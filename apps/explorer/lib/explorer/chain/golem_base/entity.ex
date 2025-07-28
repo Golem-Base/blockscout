291e99e175e177afa3b15e7af127be02211c6430
Radek Bochenek <radekb@mac.com>
2025-07-28 23:05:39 +0200

    feat: add owner

--- apps/explorer/lib/explorer/chain/golem_base/entity.ex
+++ apps/explorer/lib/explorer/chain/golem_base/entity.ex
@@ -8,19 +8,18 @@ defmodule Explorer.Chain.GolemBase.Entity do
   use Explorer.Schema
   alias Explorer.Chain.Hash
 
-  @type api? :: {:api?, true | false}
-
   @primary_key false
   typed_schema "golem_base_entities" do
     field(:key, Hash.Full, primary_key: true, null: false)
+    field(:owner, :binary, null: false)
 
     timestamps()
   end
 
   def changeset(%__MODULE__{} = golembase_entity, attrs) do
     golembase_entity
-    |> cast(attrs, [:key])
-    |> validate_required([:key])
+    |> cast(attrs, [:key, :owner])
+    |> validate_required([:key, :owner])
   end
 
   def enabled? do
