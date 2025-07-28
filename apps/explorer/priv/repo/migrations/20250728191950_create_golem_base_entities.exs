97e0049e98af1ff458aaba7991cb9bfbfffd275e
Radek Bochenek <radekb@mac.com>
2025-07-28 22:43:05 +0200

    feat: add migration to create golem_base_entities

--- apps/explorer/priv/repo/migrations/20250728191950_create_golem_base_entities.exs
+++ apps/explorer/priv/repo/migrations/20250728191950_create_golem_base_entities.exs
@@ -1,9 +1,41 @@
 defmodule Explorer.Repo.Migrations.CreateGolemBaseEntities do
   use Ecto.Migration
 
-  def change do
+  def up do
+    # Create golem_base_entity_status_type enum type if it doesn't exist
+    execute("""
+    DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'golem_base_entity_status_type') THEN
+            CREATE TYPE golem_base_entity_status_type AS ENUM ('active', 'deleted', 'expired');
+        END IF;
+    END$$;
+    """)
+
     create table(:golem_base_entities, primary_key: false) do
-      add(:key, :bytea, null: false, primary_key: true)
+      add(:key, :binary, null: false, primary_key: true)
+      add(:data, :binary)
+      add(:status, :golem_base_entity_status_type, null: false)
+      add(:owner, :binary, null: false)
+
+      add(:created_at_tx_hash, :binary)
+      add(:last_updated_at_tx_hash, :binary, null: false)
+      add(:expires_at_block_number, :bigint, null: false)
+
+      add(:inserted_at, :naive_datetime, null: false, default: fragment("now()"))
+      add(:updated_at, :naive_datetime, null: false, default: fragment("now()"))
     end
+  end
+
+  def down do
+    drop(table(:golem_base_entities))
+
+    # Drop golem_base_entity_status_type enum if it exists
+    execute("""
+    DO $$
+    BEGIN
+      DROP TYPE IF EXISTS golem_base_entity_status_type;
+    END$$
+    """)
   end
 end
