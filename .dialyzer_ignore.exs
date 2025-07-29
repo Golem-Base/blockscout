d2f2a65d489442f01d31661eb096daedf3de86f7
Radek Bochenek <radekb@mac.com>
2025-07-29 17:51:43 +0200

    chore: update :pattern_match entries for lib/explorer/chain/search.ex

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -10,7 +10,7 @@
     {"lib/explorer/smart_contract/stylus/publisher_worker.ex", :exact_eq, 14},
     {"lib/explorer/smart_contract/stylus/publisher_worker.ex", :pattern_match, 14},
     ~r/lib\/phoenix\/router.ex/,
-    {"lib/explorer/chain/search.ex", :pattern_match, 100},
-    {"lib/explorer/chain/search.ex", :pattern_match, 283},
-    {"lib/explorer/chain/search.ex", :pattern_match, 383}
+    {"lib/explorer/chain/search.ex", :pattern_match, 101},
+    {"lib/explorer/chain/search.ex", :pattern_match, 294},
+    {"lib/explorer/chain/search.ex", :pattern_match, 394}
 ]
